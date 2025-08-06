7fb98f8579e798aea70ac9290bfc5e3d9a3ceb76
Isaac Alejandro Escobar Rivadeneira <iaescobar3@espe.edu.ec>
2025-08-06 18:50:25 -0500

    eliminar y agregar imagen

--- microservicio-unidos/microservicio-Facturacion/src/main/java/espe/edu/ec/microservicio_Facturacion/repository/FacturaRepository.java
+++
@@ -1,7 +0,0 @@
-package espe.edu.ec.microservicio_Facturacion.repository;
-
-import espe.edu.ec.microservicio_Facturacion.model.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacturaRepository extends JpaRepository<Factura, Long> {
-}
--- microservicio-unidos/microservicio-central/src/main/java/espe/edu/ec/microservicio_central/repository/CosechaRepository.java
+++
@@ -1,7 +0,0 @@
-package espe.edu.ec.microservicio_central.repository;
-
-import espe.edu.ec.microservicio_central.model.Cosecha;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CosechaRepository extends JpaRepository<Cosecha, Long> {
-}
