476b49194c31447466818850dd6fd0655c21a793
Jeremy Alejandro Cuadrado Logacho <alejocuadrado1@gmail.com>
2025-08-06 13:27:18 -0500

    Cosechas

---
+++ cosechas/src/test/java/espe/edu/ec/cosechas/CosechasApplicationTests.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.cosechas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosechasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
